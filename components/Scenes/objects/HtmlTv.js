73e83f5b1a07c3e132ee4e0ad1ced79795246242
Thakisis <sub722@gmail.com>
2023-12-29 17:50:18 +0100

    added html
    To fix add reflection/shader check content size

---
+++ components/Scenes/objects/HtmlTv.js
@@ -0,0 +1,43 @@
+import { useMemo } from 'react'
+import { Html } from "@react-three/drei"
+
+import Home from '@/components/Home'
+
+export function HtmlTv({ geometry }) {
+
+    const htmlMaterial = useMemo(() => <meshStandardMaterial color="green" transparent opacity={.6} side={2} />, [])
+    return (
+        <>
+            <Html
+                castShadow
+                receiveShadow
+                occlude="blending"
+                transform
+                distanceFactor={.138}
+                style={{ display: 'flex', height: `${1500}px`, width: `${1920}px`, overflow: 'hidden', background: "red", padding: "2.5rem" }}
+                pointerEvents="none"
+                position={[-.085, .93, .21]}
+                prepend
+            >
+
+                <div
+                    style={{
+
+
+                        height: `100%`,
+                        width: `100%`,
+                        background: "yellow"
+
+
+                    }}>
+                    <Home></Home>
+
+
+                </div>
+            </Html>
+
+        </>
+    )
+
+}
+
